09b00bea7f3aff8ee7c577d29a2342fb6b1a2f9e
Abu Bakar <abubakar4232h@gmail.com>
2026-07-23 12:15:05 +0530

    digitToWord

---
+++ LAB1/fun.js
@@ -0,0 +1,41 @@
+// write a function to take any digit (0-9) and return its in words 
+// 0 - one 
+// 5 - five 
+
+function digittoword(digit){
+    switch (digit) {
+      case 0:
+        return "zero";
+
+      case 1:
+        return "one";
+
+      case 2:
+        return "two";
+
+      case 3:
+        return "three";
+
+      case 4:
+        return "four";
+
+      case 5:
+        return "five";
+
+      case 6:
+        return "six";
+
+      case 7:
+        return "seven";
+
+      case 8:
+        return "eight";
+
+      case 9:
+        return "nine";
+    }
+}
+
+console.log(digittoword(7));
+console.log(digittoword(6));
+console.log(digittoword(5));
