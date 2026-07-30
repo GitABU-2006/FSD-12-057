2212a2e319f2a6532dc29807dde1e0a81656cb58
Abu Bakar <abubakar4232h@gmail.com>
2026-07-30 11:51:14 +0530

    dom 30/7/26

--- LAB1/dom1.js
+++ LAB1/dom1.js
@@ -4,7 +4,7 @@ import { EventEmitter } from "events";
 const button = new EventEmitter();
 
 button.on("click", () => {
-  console.log("Button clicked!");
+  console.log("Button Clicked!");
 });
 
 button.emit("click");
